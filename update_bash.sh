9a9e8ff9cf0c80958d66229fe3446460b25b0c53
kbogas <bogas.ko@gmail.com>
2019-09-20 18:15:56 +0300

    Initial commit

--- update_bash.sh
+++ update_bash.sh
@@ -2,12 +2,12 @@
 
 git add .
 git commit -m "Initial commit"
-git push https://kbogas:3486kostas@kbogas/academic-kickstart.git --all
+git push https://kbogas:3486kostas@github/kbogas/academic-kickstart.git --all
 
 hugo
 cd public
 git add .
 git commit -m "Build website"
-git push https://kbogas:3486kostas@kbogas/kbogas.github.io.git
+git push https://kbogas:3486kostas@github/kbogas/kbogas.github.io.git
  --all
 cd ..
